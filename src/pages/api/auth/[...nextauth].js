73805c065c0f0f6f2cdbd8391ce1f6a1c61ab6b8
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-08-23 11:03:43 +0600

    next-auth implement

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+    providers: [
+
+    ],
+}
+
+export default NextAuth(authOptions)
